8bd11877ee86ed7337781ae1bfbac6d5e7c3d5ec
Esma Sherwani <esmasherr@gmail.com>
2020-11-26 15:45:49 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -7,14 +7,3 @@ end
 end
 
 
-loop_message_n_times = 'Hello World.'
-a = 1
-while a <=5
-puts loop_message_n_times
-  a += 1
-end
-
-def output_array 
-   ["Be yourself; everyone else is already taken. - Oscar Wilde"]
-puts output_array
-end
