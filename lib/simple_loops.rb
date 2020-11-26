84e0d37218400db1a02171b91b00a1758bfbc265
Esma Sherwani <esmasherr@gmail.com>
2020-11-26 15:28:35 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,3 +1,17 @@
+def loop_message_five_times 
+  loop_message_five_times = 'hi'
+  puts loop_message_five_times
+  puts loop_message_five_times
+  puts loop_message_five_times
+  puts loop_message_five_times
+  puts loop_message_five_times
+end
+
+
+
+
+
+
 loop_message_five_times = 'Hello World.'
 a = 1
 while a <=5
