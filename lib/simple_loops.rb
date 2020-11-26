400899f28352972e1566f6806278aaaf9aaed541
Esma Sherwani <esmasherr@gmail.com>
2020-11-26 15:52:04 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -7,7 +7,7 @@ def loop_message_five_times
   end
 end
 
-def loop_message_five_times(message, limit)
+def loop_message_n_times(message, limit)
   count = 0
   while count < limit do
     puts message
